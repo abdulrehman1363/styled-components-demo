be7faba7a74f4b123273255dbbeea3ead9cda425
abdulrehman1363 <abdulrehmanaziz256@gmail.com>
2023-03-10 17:34:00 +0500

    animation in styyled components
    animation in styyled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import logo from './logo.svg'
 import StyledButton, { FancyButton, SubmitButton } from './components/Button/Button';
+import { AnimatedLogo } from './components/Button/Button.styles';
 
 function App() {
   return (
     <div className="App">
+      <AnimatedLogo src={logo} />
       <StyledButton>Styled Button</StyledButton>
       <div>
         <br />
