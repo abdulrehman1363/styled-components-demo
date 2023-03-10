9fce5d42865506dfc994f62d41ff71c45e3942ba
abdulrehman1363 <abdulrehmanaziz256@gmail.com>
2023-03-10 17:48:50 +0500

    theming using styled components
    theming using styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg'
 import StyledButton, { FancyButton, SubmitButton } from './components/Button/Button';
-import { AnimatedLogo } from './components/Button/Button.styles';
+import { AnimatedLogo, DarkButton } from './components/Button/Button.styles';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff'
+  },
+  light: {
+    primary: '#fff',
+    text: '#000'
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <AnimatedLogo src={logo} />
-      <StyledButton>Styled Button</StyledButton>
-      <div>
-        <br />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AnimatedLogo src={logo} />
+        <StyledButton>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant="outline">Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as='a'>Fancy Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Submit</DarkButton>
       </div>
-      <StyledButton variant="outline">Styled Button</StyledButton>
-      <div>
-        <br />
-      </div>
-      <FancyButton as='a'>Fancy Button</FancyButton>
-      <div>
-        <br />
-      </div>
-      <SubmitButton>Submit</SubmitButton>
-    </div>
+    </ThemeProvider>
+
   );
 }
 
